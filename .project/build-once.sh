6905feaae2f25b9c4f17d2bfdf0359079e5001d4
deVars <r.velicaria.jr+github@gmail.com>
2023-09-28 09:07:45 -0700

    support 3p integration for friconix

--- .project/build-once.sh
+++ .project/build-once.sh
@@ -5,5 +5,6 @@ pnpm exec swc ./src --config-file ./.swcrc --out-dir ./dist ./src --delete-dir-o
 ln -s "${PWD}/index.html" ./dist/index.html
 ln -s "${PWD}/index.css" ./dist/index.css
 ln -s "${PWD}/assets" ./dist/assets
+ln -s "${PWD}/3p" ./dist/3p
 ln -s "${PWD}/src/route.yaml" ./dist/route.yaml
 ln -s "${PWD}/src/skills.yaml" ./dist/skills.yaml
--- .project/build.sh
+++ .project/build.sh
@@ -8,6 +8,7 @@ mkdir ./dist
 
 ln -s "${PWD}/index.html" ./dist/index.html
 ln -s "${PWD}/index.css" ./dist/index.css
+ln -s "${PWD}/3p" ./dist/3p
 ln -s "${PWD}/assets" ./dist/assets
 ln -s "${PWD}/src/route.yaml" ./dist/route.yaml
 ln -s "${PWD}/src/skills.yaml" ./dist/skills.yaml
