a7cd9c17e3f5173b41ffb4791e5137e074ac1f0e
deVars <r.velicaria.jr+github@gmail.com>
2023-09-28 16:59:08 -0700

    build once now does a deploy-ready version

--- .project/build-once.sh
+++ .project/build-once.sh
@@ -4,6 +4,6 @@ pnpm exec swc ./src --config-file ./.swcrc --out-dir ./dist --delete-dir-on-star
 
 ln -s "${PWD}/index.html" ./dist/index.html
 ln -s "${PWD}/index.css" ./dist/index.css
-ln -s "${PWD}/3p" ./dist/3p
-ln -s "${PWD}/assets" ./dist/assets
-ln -s "${PWD}/static" ./dist/static
+cp -r "${PWD}/3p" ./dist/3p
+cp -r "${PWD}/assets" ./dist/assets
+cp -r "${PWD}/static" ./dist/static
