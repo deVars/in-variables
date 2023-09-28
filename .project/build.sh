70191727544abaedd6fbf6683600f77d9e8c9ca5
deVars <r.velicaria.jr+github@gmail.com>
2023-09-28 16:16:47 -0700

    update build chains

--- .project/build.sh
+++ .project/build.sh
@@ -10,6 +10,7 @@ ln -s "${PWD}/index.html" ./dist/index.html
 ln -s "${PWD}/index.css" ./dist/index.css
 ln -s "${PWD}/3p" ./dist/3p
 ln -s "${PWD}/assets" ./dist/assets
+ln -s "${PWD}/static" ./dist/static
 ln -s "${PWD}/src/route.yaml" ./dist/route.yaml
 ln -s "${PWD}/src/skills.yaml" ./dist/skills.yaml
 
