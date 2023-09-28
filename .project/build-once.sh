da340e460212c60c33e180b902f137d10990ae9d
deVars <r.velicaria.jr+github@gmail.com>
2023-09-28 16:28:21 -0700

    fix bug in build-once script

--- .project/build-once.sh
+++ .project/build-once.sh
@@ -1,11 +1,9 @@
 #! /usr/bin/env bash
 
-pnpm exec swc ./src --config-file ./.swcrc --out-dir ./dist ./src --delete-dir-on-start
+pnpm exec swc ./src --config-file ./.swcrc --out-dir ./dist --delete-dir-on-start
 
 ln -s "${PWD}/index.html" ./dist/index.html
 ln -s "${PWD}/index.css" ./dist/index.css
 ln -s "${PWD}/3p" ./dist/3p
 ln -s "${PWD}/assets" ./dist/assets
 ln -s "${PWD}/static" ./dist/static
-ln -s "${PWD}/src/route.yaml" ./dist/route.yaml
-ln -s "${PWD}/src/skills.yaml" ./dist/skills.yaml
--- .project/build.sh
+++ .project/build.sh
@@ -11,8 +11,6 @@ ln -s "${PWD}/index.css" ./dist/index.css
 ln -s "${PWD}/3p" ./dist/3p
 ln -s "${PWD}/assets" ./dist/assets
 ln -s "${PWD}/static" ./dist/static
-ln -s "${PWD}/src/route.yaml" ./dist/route.yaml
-ln -s "${PWD}/src/skills.yaml" ./dist/skills.yaml
 
 pnpm exec swc ./src --config-file ./.swcrc --out-dir ./dist --watch ./src
 
