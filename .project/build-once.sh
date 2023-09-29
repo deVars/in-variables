0125d82ca2dd115d8e09e90b1efc48bce7f81df9
deVars <r.velicaria.jr+github@gmail.com>
2023-09-28 17:00:41 -0700

    fix: build-once script now outputs deploy-ready dist folder

--- .project/build-once.sh
+++ .project/build-once.sh
@@ -2,8 +2,8 @@
 
 pnpm exec swc ./src --config-file ./.swcrc --out-dir ./dist --delete-dir-on-start
 
-ln -s "${PWD}/index.html" ./dist/index.html
-ln -s "${PWD}/index.css" ./dist/index.css
+cp "${PWD}/index.html" ./dist/index.html
+cp "${PWD}/index.css" ./dist/index.css
 cp -r "${PWD}/3p" ./dist/3p
 cp -r "${PWD}/assets" ./dist/assets
 cp -r "${PWD}/static" ./dist/static
