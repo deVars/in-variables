2c07704d6c2248bfa5c687e0267b6fc9df7ace82
deVars <r.velicaria.jr+github@gmail.com>
2023-09-29 21:06:13 -0700

    add support for prod builds

---
+++ .project/build-prod.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+pnpm exec swc ./src --config-file ./.swcrc-prod --out-dir ./dist --delete-dir-on-start
+
+cp "${PWD}/index.html" ./dist/index.html
+cp "${PWD}/index.css" ./dist/index.css
+cp -r "${PWD}/3p" ./dist/3p
+cp -r "${PWD}/assets" ./dist/assets
+cp -r "${PWD}/static" ./dist/static
