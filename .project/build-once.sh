31a52c313171a90d98c054476a5b58bef6b398e4
deVars <r.velicaria.jr+github@gmail.com>
2023-10-04 09:03:55 -0700

    add custom favicon

--- .project/build-once.sh
+++ .project/build-once.sh
@@ -1,9 +1,10 @@
 #! /usr/bin/env bash
 
 pnpm exec swc ./src --config-file ./.swcrc --out-dir ./dist --delete-dir-on-start
+pnpm exec lightningcss --bundle --output-file ./dist/index.css ./index.css
 
 cp "${PWD}/index.html" ./dist/index.html
-cp "${PWD}/index.css" ./dist/index.css
+cp "${PWD}/favicon.ico" ./dist/favicon.ico
 cp -r "${PWD}/3p" ./dist/3p
 cp -r "${PWD}/assets" ./dist/assets
 cp -r "${PWD}/static" ./dist/static
