a7dda06a045a01e9e131b0eb8a60e21bb1de3e0e
deVars <r.velicaria.jr+github@gmail.com>
2023-10-13 07:49:21 -0700

    add css fallback support

--- .project/build-prod.sh
+++ .project/build-prod.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 pnpm exec swc ./src --config-file ./.swcrc-prod --out-dir ./dist --delete-dir-on-start
-pnpm exec lightningcss --bundle --minify --output-file ./dist/index.css ./index.css
+BROWSERSLIST=">= 0.25%, firefox esr, chrome > 85" pnpm exec lightningcss --browserslist --bundle --minify --output-file ./dist/index.css ./index.css
 
 cp "${PWD}/index.html" ./dist/index.html
 cp "${PWD}/friconix.min.css" ./dist/friconix.min.css
