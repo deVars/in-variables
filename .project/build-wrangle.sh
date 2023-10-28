44cad209b072843082ceb109fc866a5432e49858
deVars <r.velicaria.jr+github@gmail.com>
2023-10-28 06:57:42 -0700

    separated build chains for wrangle vs local

---
+++ .project/build-wrangle.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+pnpm exec swc ./src --config-file ./.swcrc-wrangle --out-dir ./dist --delete-dir-on-start
+BROWSERSLIST=">= 0.25%, firefox esr, chrome > 85" pnpm exec lightningcss --browserslist --bundle --minify --output-file ./dist/index.css ./index.css
+
+cp "${PWD}/index.html" ./dist/index.html
+cp "${PWD}/friconix.min.css" ./dist/friconix.min.css
+cp "${PWD}/favicon.ico" ./dist/favicon.ico
+cp -r "${PWD}/3p" ./dist/3p
+cp -r "${PWD}/assets" ./dist/assets
+cp -r "${PWD}/static" ./dist/static
