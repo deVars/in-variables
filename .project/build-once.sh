f974b6284679c4dc5ce3abda5228aa240754afc6
deVars <r.velicaria.jr+github@gmail.com>
2023-11-02 15:29:31 -0700

    remove deprecated 3p folder from builds

--- .project/build-once.sh
+++ .project/build-once.sh
@@ -6,6 +6,5 @@ pnpm exec lightningcss --bundle --output-file ./dist/index.css ./index.css
 cp "${PWD}/index.html" ./dist/index.html
 cp "${PWD}/friconix.min.css" ./dist/friconix.min.css
 cp "${PWD}/favicon.ico" ./dist/favicon.ico
-cp -r "${PWD}/3p" ./dist/3p
 cp -r "${PWD}/assets" ./dist/assets
 cp -r "${PWD}/static" ./dist/static
--- .project/build-prod.sh
+++ .project/build-prod.sh
@@ -6,6 +6,5 @@ BROWSERSLIST=">= 0.25%, firefox esr, chrome > 85" pnpm exec lightningcss --brows
 cp "${PWD}/index.html" ./dist/index.html
 cp "${PWD}/friconix.min.css" ./dist/friconix.min.css
 cp "${PWD}/favicon.ico" ./dist/favicon.ico
-cp -r "${PWD}/3p" ./dist/3p
 cp -r "${PWD}/assets" ./dist/assets
 cp -r "${PWD}/static" ./dist/static
